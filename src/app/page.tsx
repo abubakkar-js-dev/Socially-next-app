371a05af079fa1ff4b145a66362dd9f6afd01fa8
abubakkar-js-dev <mdabubakkars182@gmail.com>
2025-07-18 11:12:38 +0600

    implemented clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
+import ModeToggle from "@/components/ModeToggle";
+import { Button } from "@/components/ui/button";
 import {
   SignInButton,
-  SignUpButton,
   SignedIn,
   SignedOut,
   UserButton,
@@ -10,16 +11,15 @@ export default function Home() {
   return (
     <div>
       <SignedOut>
-        <SignInButton />
-        <SignUpButton mode="modal">
-          <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-            Sign Up
-          </button>
-        </SignUpButton>
+        <SignInButton mode="modal">
+          <Button>Sign In</Button>
+        </SignInButton>
       </SignedOut>
       <SignedIn>
         <UserButton />
       </SignedIn>
+      <ModeToggle />
+      <Button variant={"secondary"}>Click me</Button>
     </div>
   );
 }
